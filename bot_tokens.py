083ded11ccfc3f1f71504f21bb04b0459835ce9b
Chetan <chetan.kvvk@gmail.com>
2020-05-11 13:09:29 +0530

    updated tokens

--- bot_tokens.py
+++ bot_tokens.py
@@ -1,7 +1,7 @@
 from boto.s3.connection import S3Connection
 import os
 
-discord_bot_token = S3Connection(os.environ['client_key'], os.environ['S3_SECRET'])
-google_api_key = S3Connection(os.environ['google_api'], os.environ['S3_SECRET'])
-google_cse_key = S3Connection(os.environ['google_cse'], os.environ['S3_SECRET'])
-weather_api_key = S3Connection(os.environ['weather_api'], os.environ['S3_SECRET'])
+discord_bot_token = S3Connection(os.environ['client_key'])
+google_api_key = S3Connection(os.environ['google_api'])
+google_cse_key = S3Connection(os.environ['google_cse'])
+weather_api_key = S3Connection(os.environ['weather_api'])
