831354f0d45e266dbb06dca089addddc29908d3d
Chetan <chetan.kvvk@gmail.com>
2020-05-11 13:18:09 +0530

    config vars

--- bot_tokens.py
+++ bot_tokens.py
@@ -1,7 +1,7 @@
-from boto.s3.connection import S3Connection
 import os
 
-discord_bot_token = S3Connection(os.environ['client_key'])
-google_api_key = S3Connection(os.environ['google_api'])
-google_cse_key = S3Connection(os.environ['google_cse'])
-weather_api_key = S3Connection(os.environ['weather_api'])
+discord_bot_token = os.environ['client_key']
+google_api_key = os.environ['google_api']
+google_cse_key = os.environ['google_cse']
+weather_api_key = os.environ['weather_api']
+
