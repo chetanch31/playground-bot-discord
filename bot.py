95363b5580b80f84118ee5b12bc9dc22905d5065
Chetan <chetan.kvvk@gmail.com>
2020-05-13 15:08:47 +0530

    changed formatting of covid cases

--- bot.py
+++ bot.py
@@ -57,7 +57,6 @@ class misc_commands(commands.Cog, name="Miscellaneous Commands"):
         gis.search(search_params=_search_params)
         embed = discord.Embed(title=query)
         embed.set_image(url=gis.results()[0].url)
-        print(gis.results()[0].url)
         await ctx.send(embed=embed)
 
 class utility_commands(commands.Cog, name="Useful commands"):
@@ -120,13 +119,11 @@ class covid_data(commands.Cog, name="Covid Updates"):
     async def totalcases(self, ctx):
         '''Get total number of cases'''
         data = cdata.get_data_world()
-        re_str = f'''
+        re_str = '''
 
-:globe_with_meridians: **Total Confirmed Cases:** {data['TotalConfirmed']}
-
-:red_circle: **Total Deaths:** {data['TotalDeaths']}
-
-:green_circle: **Total Recoveries:** {data['TotalRecovered']}'''
+:globe_with_meridians: **Total Confirmed Cases:** {:,}
+:red_circle: **Total Deaths:** {:,}
+:green_circle: **Total Recoveries:** {:,}'''.format(data['TotalConfirmed'], data['TotalDeaths'],data['TotalRecovered'] )
 
         await ctx.send(re_str)
 
@@ -136,23 +133,29 @@ class covid_data(commands.Cog, name="Covid Updates"):
         data = cdata.get_data_by_country(country)
         country_code = data['CountryCode'].lower()
         if len(data) != 0:
-            re_str = f'''
+            re_str = '''
             
-:flag_{country_code}: **Country:** {data['Country']}
-
-:grey_exclamation: **Confirmed Cases:** {data['TotalConfirmed']}
-
-:red_circle: **Deaths: ** {data['TotalDeaths']}
-
-:green_circle: **Recoveries:** {data['TotalRecovered']}'''
+:flag_{}: **Country:** {}
+:grey_exclamation: **Confirmed Cases:** {:,}
+:red_circle: **Deaths: ** {:,}
+:green_circle: **Recoveries:** {:,}'''.format(country_code, data['Country'], data['TotalConfirmed'], data['TotalDeaths'], data['TotalRecovered'])
             await ctx.send(re_str)
         else:
             ctx.send("**No data found for the given country**")
+
+class VoiceCommands(commands.Cog, name="Music Commands"):
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        '''Joins a voice channel'''
+        
+        ctx.voice_client.move_to(channel)        
+        await channel.connect(60)
 
 
 client.add_cog(meme_commands())
 client.add_cog(utility_commands())
 client.add_cog(misc_commands())
 client.add_cog(covid_data())
+client.add_cog(VoiceCommands())
 
 client.run(discord_bot_token)
